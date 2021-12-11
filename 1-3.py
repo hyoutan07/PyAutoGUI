7b7134a3b0752c5f786da926da6c0680aa1ec07b
hyoutan07 <hyoutan0726@gmail.com>
2021-12-11 22:32:46 +0900

    add 1-3.py file

---
+++ 1-3.py
@@ -0,0 +1,11 @@
+import pyautogui as pg
+import time
+
+pg.moveTo(10,10,duration=1)
+time.sleep(1)
+pg.moveTo(0,500,duration=1)
+time.sleep(1)
+pg.moveTo(500,0,duration=1)
+time.sleep(1)
+pg.moveTo(500,500,duration=1)
+time.sleep(1)
